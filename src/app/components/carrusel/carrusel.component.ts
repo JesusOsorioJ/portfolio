d4807986d02dd0f0ca1d053b76a2fe1c7ef479ab
unknown <jesdaos@hotmail.com>
2023-02-11 11:24:21 -0500

    detele btn components

--- src/app/components/carrusel/carrusel.component.ts
+++ src/app/components/carrusel/carrusel.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap'; 
-
+import { databases } from "../../pages/details/database"
 @Component({
   selector: 'app-carrusel',
   templateUrl: './carrusel.component.html'
 })
 export class CarruselComponent implements OnInit {
 
-  @Input() fotos = {url:"",imageswebpages:[]}
-  @Input() pos : number = 0;
+  @Input() step : number = 0;
+  data = databases
 
   constructor(private _config:NgbCarouselConfig){
+
   }
   ngOnInit() {
     
