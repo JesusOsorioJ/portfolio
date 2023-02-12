47dca57af5a19bc8303b29b45ee01454949a9fbd
unknown <jesdaos@hotmail.com>
2023-02-11 21:43:22 -0500

    FIxed components error

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent {
   parrafo: Array<string> = ["Soy","un","programador","FullStack","con","Enfasis","en","el","Stack",
    "Mern","(MongoDB,","Express.js,","React.js","y","Nodejs)","con", "aptitud", "de", 
    "aprender" ,"nuevas", "tecnologias.","Conoce", "mis" ,"proyectos", "dando" ,
-   "click", "debajo", "o" ,"en" ,"la" ,"parte", "superior."]
+   "click", "debajo", "o" ,"desliza" ,"hacia" ,"abajo"]
   ph : number = 0;
   h : number = window.innerHeight;
   p : number = 0
@@ -21,61 +21,73 @@ export class HomeComponent {
   async ngOnInit(){
     let tl = gsap.timeline()
 
-    tl.to("#landing",0, {backgroundColor:'#B0C4DE'})
-    tl.to("#svg",0.0, {fill: '#FFFFFF'})
-    tl.to("#svg", 0.8, {x:'20vw', ease:"none"},0)
-    tl.to("#svg", 0.8, {y:"50vh", ease:"power1.in"},0)
-    tl.to("#landing",0.2, {backgroundColor:'#FFB6C1'},0.8)
-    tl.to("#svg", 0.56, {x:'34vw', ease:"none"},0.8)
-    tl.to("#svg", 0.56, {y:"25vh", ease:"power1.out"},0.8)
-    tl.to("#svg", 0.56, {x:'48vw',ease:"none"},1.36)
-    tl.to("#svg", 0.56, {y:"50vh",ease:"power1.in"},1.36)
-    tl.to("#landing",0.3, {backgroundColor:'#F0E68C'},1.92)
-    tl.to("#svg", 0.39, {x:'56.4vw', ease:"none"},1.92)
-    tl.to("#svg", 0.39, {y:"37.5vh", ease:"power1.out"},1.92)
-    tl.to("#svg", 0.39, {x:'64.8vw', ease:"none"},2.31)
-    tl.to("#svg", 0.39, {y:"50vh", ease:"power1.in"},2.31)
-    tl.to("#landing",0.3, {backgroundColor:'#FFFFFF'})
-    tl.to("#svg", 1, {opacity:0}, 2.70,)
+    // tl.to("#contactobtn",0, {y:"10vh"},0) 
+    tl.to("#headerword",3, {y:"0vh"},0) 
+
+    // tl.to("#parrafonombre",0, {display:"none"},0)
+    // tl.to("#landing",0, {backgroundColor:'#B0C4DE'})
+    // tl.to("#svg",0.0, {fill: '#FFFFFF'})
+    // tl.to("#svg", 0.8, {x:'20vw', ease:"none"},0)
+    // tl.to("#svg", 0.8, {y:"50vh", ease:"power1.in"},0)
+    // tl.to("#landing",0.2, {backgroundColor:'#FFB6C1'},0.8)
+    // tl.to("#svg", 0.56, {x:'34vw', ease:"none"},0.8)
+    // tl.to("#svg", 0.56, {y:"25vh", ease:"power1.out"},0.8)
+    // tl.to("#svg", 0.56, {x:'48vw',ease:"none"},1.36)
+    // tl.to("#svg", 0.56, {y:"50vh",ease:"power1.in"},1.36)
+    // tl.to("#landing",0.3, {backgroundColor:'#F0E68C'},1.92)
+    // tl.to("#svg", 0.39, {x:'56.4vw', ease:"none"},1.92)
+    // tl.to("#svg", 0.39, {y:"37.5vh", ease:"power1.out"},1.92)
+    // tl.to("#svg", 0.39, {x:'64.8vw', ease:"none"},2.31)
+    // tl.to("#svg", 0.39, {y:"50vh", ease:"power1.in"},2.31)
+    // tl.to("#landing",0.3, {backgroundColor:'#FFFFFF'})
+    // tl.to("#svg", 1, {opacity:0}, 2.70,)
+    // tl.to("#parrafonombre",0, {display:"block"})
     
     
     
     setTimeout(() => {
       
-    tl.to("#palabra2",0, {color:"white"},0)
-    tl.to("#palabra4",0, {color:"white"},0)
+    // tl.to("#palabra2",0, {color:"white"},0)
+    // tl.to("#palabra4",0, {color:"white"},0)
+    
 
-    tl.to("#names",0, {color:"#FFFFFF", },2.31)
-    tl.to("#names",0,  {fontFamily: "SegoeUI", left:'25vw'})
-    tl.to("#names", 0.2, {color:"black",x:'30vw'})    
-    tl.to("#names", 0.2, {y:'3vh', stagger:.1, ease:"power1.inOut"})
-    tl.to("#names", 0.33, {y:'-5vh', stagger:.1, ease:"power1.out" },"-=1.2")
-    tl.to("#names", 0.29, {y:'1vh', stagger:.1 ,ease:"power1.inOut"},"-=1.2")
-    tl.to("#names", 0, {stagger:.1, fontFamily:"TrebuchetMS"},"-=1.2")
-    tl.to("#names", 0.12, {y:'0vh', stagger:.1, ease:"power1.in"},"-=1.2")
-    tl.to("#names", 0.2, {y:'3vh', ease:"power1.inOut"})
-    tl.to("#names", 0.34, {y:'-6vh', fontFamily: "SegoeUI", ease:"power1.out"})
-    tl.to("#names", 0.33, {y:'2vh',ease:"power1.in"})
-    tl.to("#names", 0.23, {y:'-2vh',  ease:"power1.inOut"})
-    tl.to("#names", 0.2, {y:'1vh',  fontFamily: "SegoeUI", ease:"power1.out"})
-    tl.to("#names", 0.12, {y:'0vh', ease:"power1.in"})
-   
-    tl.to("#palabra2",0.05, {color:"grey",yoyo:true, repeat:1})
-    tl.to("#palabra2",0.05, {color:"grey",yoyo:true, repeat:1},"+=0.1")
-    tl.to("#palabra2",0.05, {color:"#D3D3D3"},"+=0.1")
-    tl.to("#palabra2",0.05, {color:"#778899",yoyo:true, repeat:1},"+=1") 
-    tl.to("#palabra2",0.05, {color:"#778899",yoyo:true, repeat:1},"+=0.5")
-    tl.to("#palabra2",0.05, {color:"#778899"},"+=0.5")
-    tl.to("#palabra2",3, {color:"black"},"+=0.1") 
     
-    tl.to("#palabra4",0.05, {color:"grey",yoyo:true, repeat:1},"-=5.2")
-    tl.to("#palabra4",0.05, {color:"grey",yoyo:true, repeat:1},"+=0.1")
-    tl.to("#palabra4",0.05, {color:"#D3D3D3"},"+=0.2")
-    tl.to("#palabra4",0.05, {color:"#778899",yoyo:true, repeat:1},"+=1") 
-    tl.to("#palabra4",0.05, {color:"#778899",yoyo:true, repeat:1},"+=0.5")
-    tl.to("#palabra4",0.05, {color:"#778899"},"+=0.5")
-    tl.to("#palabra4",3, {color:"black"},"+=0.1") 
+
+    // tl.to("#names",0, {color:"#FFFFFF", x:'-8vw' },0)
+    // tl.to("#minombre",0, {color:"#FFFFFF",x:'-8vw' },0)
+
+    // tl.to("#names", 0.3, {color:"black",x:'8vw'}) 
+    // tl.to("#minombre",0.3, {color:"#0000CD",x:'8vw'}, "-=0.2")   
+    // tl.to("#names", 0.2, {y:'3vh', stagger:.1, ease:"power1.inOut"})
+    // tl.to("#names", 0.33, {y:'-5vh', stagger:.1, ease:"power1.out" },"-=1.2")
+    // tl.to("#names", 0.29, {y:'1vh', stagger:.1 ,ease:"power1.inOut"},"-=1.2")
+    // tl.to("#names", 0, {stagger:.1, fontFamily:"TrebuchetMS"},"-=1.2")
+    // tl.to("#names", 0.12, {y:'0vh', stagger:.1, ease:"power1.in"},"-=1.2")
+    // tl.to("#names", 0.2, {y:'3vh', ease:"power1.inOut"})
+    // tl.to("#names", 0.34, {y:'-6vh', fontFamily: "SegoeUI", ease:"power1.out"})
+    // tl.to("#names", 0.33, {y:'2vh',ease:"power1.in"})
+    // tl.to("#names", 0.23, {y:'-2vh',  ease:"power1.inOut"})
+    // tl.to("#names", 0.2, {y:'1vh',  fontFamily: "SegoeUI", ease:"power1.out"})
+    // tl.to("#names", 0.12, {y:'0vh', ease:"power1.in"})
+
+    // tl.to("#palabra2",0.05, {color:"grey",yoyo:true, repeat:1})
+    // tl.to("#palabra2",0.05, {color:"grey",yoyo:true, repeat:1},"+=0.1")
+    // tl.to("#palabra2",0.05, {color:"#D3D3D3"},"+=0.1")
+    // tl.to("#palabra2",0.05, {color:"#778899",yoyo:true, repeat:1},"+=1") 
+    // tl.to("#palabra2",0.05, {color:"#778899",yoyo:true, repeat:1},"+=0.5")
+    // tl.to("#palabra2",0.05, {color:"#778899"},"+=0.5")
+    // tl.to("#palabra2",3, {color:"black"},"+=0.1") 
+
+    // tl1.to("#palabra4",0.05, {color:"grey"},"-=4.0")
+    // tl1.to("#palabra4",0.05, {color:"grey",yoyo:true, repeat:1},"+=1")
+    // tl1.to("#palabra4",0.05, {color:"#D3D3D3"},"-=0.2")
+    // tl1.to("#palabra4",0.05, {color:"#778899",yoyo:true, repeat:1},"+=1") 
+    // tl1.to("#palabra4",0.05, {color:"#778899",yoyo:true, repeat:1},"+=0.5")
+    // tl1.to("#palabra4",0.05, {color:"#778899"},"+=0.5")
+    // tl1.to("#palabra4",3, {color:"black"},"+=0.1") 
     
+    // tl.to("#contactobtn",3, {y:"0vw"},">")
+    // tl.to("#headerword",3, {y:"0vh"},"-=3.0") 
     
     }, 1)
 
@@ -84,15 +96,25 @@ export class HomeComponent {
   
   public onScroll(){
     this.ph = window.pageYOffset/this.h
-    // if (this.ph-this.p>=0.3&&this.p!=this.ph){
-    //   window.scroll(0, Math.ceil(this.ph)*this.h);
-    //   this.p = this.ph
-    // }
-    // if (this.p-this.ph>=0.3&&this.p!=this.ph){
-    //   window.scroll(0, Math.floor(this.ph)*this.h);
-    //   this.p = this.ph
-    // }
-  
+
+
+    if (this.ph-this.p>=0.3&&this.p!=this.ph){
+      window.scroll(0, Math.ceil(this.ph)*this.h);
+      this.p = this.ph
+      let vh = Math.ceil(this.ph)*100+"vh"
+      let vhi = (Math.ceil(this.ph)*100-10)+"vh"
+      gsap.to("#headerword",1, {y:vhi})  
+      gsap.to("#headerword",1.5, {y:vh})      
+    }
+    if (this.p-this.ph>=0.3&&this.p!=this.ph){
+      window.scroll(0, Math.floor(this.ph)*this.h);
+      this.p = this.ph
+      let vh = Math.floor(this.ph)*100+"vh"
+      let vhi = (Math.floor(this.ph)*100-10)+"vh" 
+      gsap.to("#headerword",0.0, {y:vhi})  
+      gsap.to("#headerword",1.5, {y:vh}) 
+    }
+    
   }
   
 }
