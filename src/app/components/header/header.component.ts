4eebf14e57b5ce661446d97866f9cca83f0cbffe
unknown <jesdaos@hotmail.com>
2023-02-23 23:26:48 -0500

    add te last ork in febrery 2023

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,29 +7,55 @@ import { gsap } from 'gsap';
 })
 export class HeaderComponent {
   show : boolean = false
-  ph : number = 0;
-  h : number = window.innerHeight;
-  w : number = window.innerWidth;
+  p : number = 0;
   
-  async ngOnInit(){
+//   async ngOnInit(){
   
-  gsap.to(["#headerword","#headericon"],0, {y:"-15vh", 
-  color:'rgb(10, 25, 47),',fill:'rgb(10, 25, 47)', opacity:"100"}) 
-  gsap.to(["#headerword","#headericon"],2, {y:"0vh",fill:'#8892b0',
-  opacity:"100",delay:6 }) 
+//   gsap.to(["#headerword","#headericon"],0, {y:"-15vh", 
+//   color:'rgb(10, 25, 47),',fill:'rgb(10, 25, 47)', opacity:"100"}) 
+//   gsap.to(["#headerword","#headericon"],2, {y:"0vh",fill:'#8892b0',
+//   opacity:"100",delay:6 }) 
   
   
-}
-
-  public onScroll(){
-    this.ph = window.pageYOffset/this.h
-    console.log("h",this.h)
-
-  }
-
+// }
   public showClose(){
     this.show=!this.show
     
   }
 
+
+  ngOnInit() {
+    // Animation open and close header 
+    setInterval(() => {
+      //header quiet at the top
+      if (window.pageYOffset / window.innerHeight < 0.1) {
+        gsap.to(["#headerword", "#headericon"],
+          { color: "white", duration: 2, stroke: "white" })
+        gsap.to("#header", {
+          background: "transparent", duration: 2,
+        })
+      } else if (window.pageYOffset / window.innerHeight != this.p) {
+        //header quiet  but not at the top
+        gsap.to(["#headerword", "#headericon"],
+          { color: "gray", stroke: "gray", duration: 2 })
+        gsap.to("#header", {
+          background: "#1d3a65", duration: 2,
+        })
+      }
+      this.p = window.pageYOffset / window.innerHeight
+    }, 1000)
+  }
+
+  onScroll() {
+    //Header in move
+    if (window.pageYOffset / window.innerHeight > 0.1) {
+      gsap.to(["#headerword", "#headericon"], {
+          color: "transparent",
+        duration: 2, stroke: "transparent"
+      })
+      gsap.to("#header", {
+        background: "transparent", duration: 2,
+      })
+
+    }}
 }
