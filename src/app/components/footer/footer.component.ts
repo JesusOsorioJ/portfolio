d45b6509f9eef76bc249fcc2d96f2272bf0aeb62
unknown <jesdaos@homail.com>
2023-03-01 16:27:28 -0500

    solved error of styles tailwind

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,29 +1,9 @@
 import { Component } from '@angular/core';
-import { gsap } from 'gsap'
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
- ngOnInit(){
-  gsap.registerPlugin(ScrollTrigger);
-
-  gsap.to("#target", {
-      filter: " opacity(100%)", y: "-2vh", duration: 1, stagger: 0.2,
-      scrollTrigger: {
-        trigger: "#target", start: "center 80%", end: "center 0%", 
-        toggleActions: "play none none none",
-       
-      },
-    });
- }
+ 
 }
-
-
- // markers: {
-        //     startColor: "purple",
-        //     endColor: "fuchsia",
-        //     fontSize: "3rem",
-        //   },
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,35 @@
 import { Component  } from '@angular/core';
+import { gsap } from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  
+ 
+ngOnInit(){
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to("#target", {
+      filter: " opacity(100%)", y: "-2vh", duration: 1, stagger: 0.2,
+      scrollTrigger: {
+        trigger: "#target", start: "center 80%", end: "center 0%", 
+        toggleActions: "play none none none",
+        markers: {
+              startColor: "purple",
+              endColor: "fuchsia",
+              fontSize: "3rem",
+            },
+       
+      },
+    });
+ }
   
 }
 
+// markers: {
+        //     startColor: "purple",
+        //     endColor: "fuchsia",
+        //     fontSize: "3rem",
+        //   },
