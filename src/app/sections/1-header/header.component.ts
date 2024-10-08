57538f16c589b21dacf4c912300f7c7e32c15472
Jesus Osorio <josorio@disruptivestudio.com>
2024-10-07 19:44:18 -0500

    Mejorar consumo i18

---
+++ src/app/sections/1-header/header.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  show : boolean = false
+  p : number = 0;
+  link = [
+    {href: "#inicio", iconname:"home", translate: "home"},
+    {href: "#experience", iconname:"experience", translate: "workExperience"},
+    {href: "#proyects", iconname:"myProjects", translate: "myProjects"},
+    {href: "#contact", iconname:"contact", translate: "contact"},
+
+  ]
+  themeValue = "dark"
+  language = "en"
+  
+  constructor(private translate: TranslateService) {
+    const savedLanguage = localStorage.getItem('language') || 'en';
+    translate.setDefaultLang('en');
+    this.language = savedLanguage;
+    this.translate.use(this.language); // Configura el idioma seleccionado
+
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    this.themeValue = savedTheme;
+    document.documentElement.setAttribute('data-theme', this.themeValue); // Configura el tema seleccionado
+  }
+
+  public showClose(){
+    this.show=!this.show
+  }
+
+  switchLanguage(value: string) {
+    this.translate.use(value);
+    this.language = value;
+    localStorage.setItem('language', value);
+  }
+
+  switchTheme() {
+    let value = "dark"
+    if (this.themeValue == "dark") value = "light"
+    document.documentElement.setAttribute('data-theme', value);
+    this.themeValue = value;
+    localStorage.setItem('theme', value);
+  }
+}
---
+++ src/app/sections/4-proyects/proyects.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { databases } from '../../pages/details/database'
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './proyects.component.html'
+})
+export class InputComponent implements OnInit {
+  data = databases
+  valueObjet =""
+  valueObjet1 =""
+  myControl = new FormControl('');
+  options: string[] = ["javascript","react","node","express","typescript","graphql","angular","nextjs","nodemon","mongodb","mysql","sass",
+  "tailwind","redux","socket","sendgrid","cloudinary","stripe",
+  "reactrouter","firebase","fbauthentication","gsap","scrolltrigger"];
+  filteredOptions!: Observable<string[]>;
+ 
+  onSubmit() {
+    this.valueObjet1 = this.valueObjet
+    this.data = []
+    for (let i of databases){
+      for (let j of i.technologies.all) {
+        if (j == this.valueObjet){
+          this.data=[...this.data, i]
+        }
+      }
+    }
+    if (this.valueObjet.length == 0){this.data = databases}
+  }
+  
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    this.valueObjet = filterValue
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+}
