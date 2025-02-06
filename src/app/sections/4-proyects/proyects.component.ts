72ae64b9a5decebbb6ad17e171d786a65ac30535
Jesus Osorio <josorio@disruptivestudio.com>
2025-02-05 21:29:21 -0500

    Pagina de proyectos

--- src/app/sections/4-proyects/proyects.component.ts
+++ src/app/sections/4-proyects/proyects.component.ts
@@ -9,68 +9,41 @@ import { databases } from '../../pages/details/database'
   templateUrl: './proyects.component.html'
 })
 export class InputComponent implements OnInit {
-  // @ViewChildren('proyecttarget') proyecttarget!: QueryList<ElementRef>;
-  data = databases
-  valueObjet =""
-  valueObjet1 =""
+  data = databases;
+  paginatedData: any[] = [];
+  valueObjet = "";
+  valueObjet1 = "";
   myControl = new FormControl('');
   options: string[] = [
-    'JS',
-    'TS',
-    'PHP',
-
-    'react',
-    'nextjs',
-    'angular',
-    'vite',
-    'vuejs', 
-    
-    'tailwind',
-    'sass',
-    'bootstrap',
-    'gapsap',
-    'eslint',
-        
-    'vuex',
-    'redux',
-    'zustand',
-
-    'node',
-    'nestjs',
-    'laravel',
-    'express',
-    'graphql', 
-    'firebase',
-    
-    'stripe',
-    'socket',
-    'sendgrid',
-    'cloudinary',
-    'bcryptjs',
-    'swagger',
-    
-    'postgresql',
-    'prisma',
-    'mysql',
-    'mongodb', 
-
-    'jest',
-    'cypress',
-    'PHPunit',
+    'JS', 'TS', 'PHP',
+    'react', 'nextjs', 'angular', 'vite', 'vuejs', 
+    'tailwind', 'sass', 'bootstrap', 'gapsap', 'eslint',
+    'vuex', 'redux', 'zustand',
+    'node', 'nestjs', 'laravel', 'express', 'graphql', 'firebase',
+    'stripe', 'socket', 'sendgrid', 'cloudinary', 'bcryptjs', 'swagger',
+    'postgresql', 'prisma', 'mysql', 'mongodb', 
+    'jest', 'cypress', 'PHPunit'
   ];
   filteredOptions!: Observable<string[]>;
+
+  currentPage: number = 1;
+  itemsPerPage: number = 6;
+  totalPages: number = 1;
  
   onSubmit() {
-    this.valueObjet1 = this.valueObjet
-    this.data = []
-    for (let i of databases){
+    this.valueObjet1 = this.valueObjet;
+    this.data = [];
+    for (let i of databases) {
       for (let j of i.technologies.all) {
-        if (j == this.valueObjet){
-          this.data=[...this.data, i]
+        if (j == this.valueObjet) {
+          this.data = [...this.data, i];
         }
       }
     }
-    if (this.valueObjet.length == 0){this.data = databases}
+    if (this.valueObjet.length == 0) {
+      this.data = databases;
+    }
+    this.updatePagination();
   }
   
   ngOnInit() {
@@ -78,25 +51,31 @@ export class InputComponent implements OnInit {
       startWith(''),
       map(value => this._filter(value || '')),
     );
-
-    // this.proyecttarget.forEach((box:any) => {
-    //   const scrollBox = gsap.timeline({
-    //     scrollTrigger: {
-    //       trigger: box,
-    //       pin: true,
-    //       start: "medium top",
-    //       end: "bottom bottom",
-    //       markers: true,
-    //       toggleActions: "play none none reverse"
-    //     }
-    //   });
-    //   scrollBox.from(box, { duration: 1, opacity: 0 });
-    // });
+    this.updatePagination();
   }
 
   private _filter(value: string): string[] {
-    const filterValue = value;
-    this.valueObjet = filterValue
+    const filterValue = value.toLowerCase();
+    this.valueObjet = filterValue;
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  updatePagination() {
+    this.totalPages = Math.ceil(this.data.length / this.itemsPerPage);
+    this.paginatedData = this.data.slice((this.currentPage - 1) * this.itemsPerPage, this.currentPage * this.itemsPerPage);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePagination();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagination();
+    }
   }
 }
